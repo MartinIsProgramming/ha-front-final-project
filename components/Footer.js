efffbf40f2bb54860ac6a9bc64372ab8f5a54882
Martin Vera <martin91.vera@gmail.com>
2022-03-21 18:15:24 -0300

    add footer

---
+++ components/Footer.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+
+const navigation = [
+  { label: 'home', href: '/' },
+  { label: 'about', href: '/about' },
+  { label: 'cars', href: '/cars' },
+];
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800">
+      <div className="flex flex-col items-center py-12 mx-auto space-y-4 max-w-7xl">
+        <Link href="/">
+          <a className="text-2xl text-white">CARSHOP</a>
+        </Link>
+        <ul className="flex items-center justify-center">
+          {navigation.map(({ label, href }) => (
+            <li
+              className="py-2 mx-3 text-white rounded hover:underline"
+              key={label}
+            >
+              <Link href={href}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+        <p className="mt-8 text-base text-center text-gray-400">
+          &copy; {`${new Date().getFullYear()}`} Carshop, Inc. All rights
+          reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
