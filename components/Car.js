65b8e34b15f1ba575a915a33f5d6c5e6b6e91f12
Martin Vera <martin91.vera@gmail.com>
2022-03-23 17:06:41 -0300

    reusable car component

---
+++ components/Car.js
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import StandardBtn from './shared/StandardBtn';
+import Rating from './Rating';
+
+const Car = ({ car }) => {
+  console.log(car.rating);
+  return (
+    <div className="lg:max-w-3xl">
+      <div className="relative w-full h-56 overflow-hidden rounded-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 lg:w-2/3 lg:h-64">
+        <Image
+          src={car.image}
+          alt="car"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </div>
+
+      <div className="my-3 lg:flex lg:justify-between">
+        <h3 className="mb-1 text-lg font-medium text-primary-brand-dark">
+          {car.brand} {car.model}
+        </h3>
+        <div className="flex items-center space-x-1 text-primary-brand-light">
+          <span>{car.year} |</span>
+          <span>USD {car.price_usd} |</span>
+          <Rating rating={car.rating} />
+        </div>
+      </div>
+      <p className="text-primary-brand-light">{car.description}</p>
+      <div className="flex mt-3 space-x-2">
+        <StandardBtn text="buy car" customClasses="w-28" />
+        <StandardBtn text="more info" isSecondary customClasses="w-28" />
+      </div>
+    </div>
+  );
+};
+
+export default Car;
--- pages/cars.js
+++ pages/cars.js
@@ -1,59 +1,17 @@
-import { StarIcon as FullStar } from '@heroicons/react/solid';
-import { StarIcon as EmptyStar } from '@heroicons/react/outline';
 import Filter from '../components/Filter';
 import SectionTitle from '../components/shared/SectionTitle';
-import StandardBtn from '../components/shared/StandardBtn';
-import Image from 'next/image';
+import Car from '../components/Car';
 
 const CarsPage = ({ carsData }) => {
-  console.log(carsData);
   return (
     <section className="px-4 py-12 mx-auto sm:py-20 sm:px-6 lg:px-8 max-w-7xl">
       <SectionTitle title="Search your future car" label="collection" />
       <div className="grid mt-4 gap-y-6 md:grid-cols-3 lg:grid-cols-4 md:gap-x-4">
         <Filter />
         <div className="space-y-12 md:col-span-2 lg:col-span-3">
-          {carsData.map((car, i) => {
-            return (
-              <div key={i} className="lg:max-w-3xl">
-                <div className="relative w-full h-56 overflow-hidden rounded-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 lg:w-2/3 lg:h-64">
-                  <Image
-                    src={car.image}
-                    alt="car"
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="center"
-                  />
-                </div>
-
-                <div className="my-3 lg:flex lg:justify-between">
-                  <h3 className="mb-1 text-lg font-medium text-primary-brand-dark">
-                    {car.brand} {car.model}
-                  </h3>
-                  <div className="flex items-center space-x-1 text-primary-brand-light">
-                    <span>{car.year} |</span>
-                    <span>USD {car.price_usd} |</span>
-                    <span className="flex">
-                      <FullStar className="w-4 h-4 text-yellow-400" />
-                      <EmptyStar className="w-4 h-4" />
-                      <EmptyStar className="w-4 h-4" />
-                      <EmptyStar className="w-4 h-4" />
-                      <EmptyStar className="w-4 h-4" />
-                    </span>
-                  </div>
-                </div>
-                <p className="text-primary-brand-light">{car.description}</p>
-                <div className="flex mt-3 space-x-2">
-                  <StandardBtn text="buy car" customClasses="w-28" />
-                  <StandardBtn
-                    text="more info"
-                    isSecondary
-                    customClasses="w-28"
-                  />
-                </div>
-              </div>
-            );
-          })}
+          {carsData.map((car, i) => (
+            <Car key={i} car={car} />
+          ))}
         </div>
       </div>
     </section>
