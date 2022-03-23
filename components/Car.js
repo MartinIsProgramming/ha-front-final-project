c78d5888e79a95783997f68663332513c8d97a8e
Martin Vera <martin91.vera@gmail.com>
2022-03-23 17:30:47 -0300

    dynamic badges and prices format

--- components/Car.js
+++ components/Car.js
@@ -3,6 +3,12 @@ import StandardBtn from './shared/StandardBtn';
 import Rating from './Rating';
 
 const Car = ({ car }) => {
+  const renderNewBadge = car.status === 1 && (
+    <span className="absolute px-2 py-1 text-xs font-medium text-gray-100 rounded-md top-3 left-2 bg-brand-orange">
+      new
+    </span>
+  );
+
   return (
     <div className="lg:max-w-3xl">
       <div className="relative w-full h-56 overflow-hidden rounded-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 lg:w-2/3 lg:h-64">
@@ -13,6 +19,7 @@ const Car = ({ car }) => {
           objectFit="cover"
           objectPosition="center"
         />
+        {renderNewBadge}
       </div>
 
       <div className="my-3 lg:flex lg:justify-between">
@@ -21,7 +28,9 @@ const Car = ({ car }) => {
         </h3>
         <div className="flex items-center space-x-1 text-primary-brand-light">
           <span>{car.year} |</span>
-          <span>USD {car.price_usd} |</span>
+          <span>
+            USD {car.price_usd.toLocaleString({ maximumFractionDigits: 2 })} |
+          </span>
           <Rating rating={car.rating} />
         </div>
       </div>
--- pages/cars.js
+++ pages/cars.js
@@ -5,7 +5,7 @@ import Car from '../components/Car';
 const CarsPage = ({ carsData }) => {
   return (
     <section className="px-4 py-12 mx-auto sm:py-20 sm:px-6 lg:px-8 max-w-7xl">
-      <SectionTitle title="Search your future car" label="collection" />
+      <SectionTitle title="Search your car" label="collection" />
       <div className="grid mt-4 gap-y-6 md:grid-cols-3 lg:grid-cols-4 md:gap-x-4">
         <Filter />
         <div className="space-y-12 md:col-span-2 lg:col-span-3">
