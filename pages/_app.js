236be89a4e5d087491cbe6ea257e650357d3de7c
Martin Vera <martin91.vera@gmail.com>
2022-03-25 17:18:08 -0300

    add prop-types for MyApp

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import Navigation from '../components/layout/Navigation';
 import Footer from '../components/layout/Footer';
+import propTypes from 'prop-types';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,5 +12,10 @@ function MyApp({ Component, pageProps }) {
     </>
   );
 }
+
+MyApp.propTypes = {
+  Component: propTypes.func.isRequired,
+  pageProps: propTypes.any.isRequired,
+};
 
 export default MyApp;
